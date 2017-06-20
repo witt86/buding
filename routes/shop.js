1d3464a269a72d2d8d616ae0e7c5ebc72a883e71
cloud-z <threepigs@vip.qq.com>
2017-06-20 18:23:34 +0800

    Merge branch 'master' of https://github.com/witt86/buding into master-zed
    # Conflicts:
    #	.babelrc
    #	app.js

--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 
+import TMSProductAPI from './../server/lib/TMSProductAPI';
+
 import DataModel from './../server/model/DataModel';
 
 const router = new Router();
@@ -10,15 +12,48 @@ router.get('/shopHome', async (req, res, next) => {
 
         rs.title = '布丁酒店';
 
-        rs.sections = [{title:'特产美食',url:'food',products:[{name:'HUROM进口榨汁机',retail_price:532},{name:'HUROM进口榨汁机',retail_price:532},{name:'HUROM进口榨汁机',retail_price:532},{name:'HUROM进口榨汁机',retail_price:532}]},
-            {title:'手机数码',url:'mobile',products:[{name:'HUROM进口榨汁机',retail_price:532},{name:'HUROM进口榨汁机',retail_price:532},{name:'HUROM进口榨汁机',retail_price:532},{name:'HUROM进口榨汁机',retail_price:532}]},
-            {title:'护肤用品',url:'skinCare',products:[{name:'HUROM进口榨汁机',retail_price:532},{name:'HUROM进口榨汁机',retail_price:532},{name:'HUROM进口榨汁机',retail_price:532},{name:'HUROM进口榨汁机',retail_price:532}]},
-            {title:'家用电器',url:'appliance',products:[{name:'HUROM进口榨汁机',retail_price:532},{name:'HUROM进口榨汁机',retail_price:532},{name:'HUROM进口榨汁机',retail_price:532},{name:'HUROM进口榨汁机',retail_price:532}]}];
+        rs.activityProducts = await DataModel.ProductSource.findAll({
+            where:{
+                tags:{
+                    like:'%布丁首页%'
+                }
+            },
+            limit:4
+        });
+
+        let categorys = await DataModel.ProductCategory.findAll();
+        categorys = JSON.parse(JSON.stringify(categorys));
+        for(let category of categorys){
+            category.products = await DataModel.ProductSource.findAll({
+                where:{
+                    productcategoryId:category.id,
+                    status:1
+                },
+                limit:5
+            });
+        }
+        rs.sections = categorys;
 
         // console.log(rs.category[0].productList);
         res.render('shop/shopHome', rs);
     }catch (e){
         console.log('-----e:/shopHome-----');
+        console.log(e);
+    }
+});
+
+
+router.get('/productDetail/:code', async (req, res, next) => {
+    try{
+        let [rs,code] = [{},req.params.code];
+
+        rs.productInfo = await TMSProductAPI('get_product',{code:code});
+
+        rs.title = rs.productInfo.name;
+
+        res.render('shop/productDetail', rs);
+    }catch (e){
+        console.log('-----e:/productDetail-----');
         console.log(e);
     }
 });
